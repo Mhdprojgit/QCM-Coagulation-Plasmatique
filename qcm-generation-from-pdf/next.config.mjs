9756076087f1b64302e72e18dae5e527ce569ac1
Mhdprojgit <mehdiafetude@gmail.com>
2026-05-18 19:46:41 +0100

    Update Next.js configuration for export and basePath

--- qcm-generation-from-pdf/next.config.mjs
+++ qcm-generation-from-pdf/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/QCM-Coagulation-Plasmatique',
+
   typescript: {
     ignoreBuildErrors: true,
   },
